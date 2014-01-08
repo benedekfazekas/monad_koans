3c2bd23b9fa2bc94d4fb73dcf990c53415dcaeae
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2014-01-08 13:14:58 +0000

    bumps up version adds description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject monad-koans "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject monad-koans "1.0.0"
+  :description "Set of exercises (koans) to learn monads in clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [koan-engine "0.2.0"]
                  [org.clojure/algo.monads "0.1.4"]]
